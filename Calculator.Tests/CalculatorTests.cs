735498ec5a973ec17802565ec17d4a49c97965f1
Bartłomiej Płoszyński <b.ploszynski@gmail.com>
2018-03-11 19:18:05 +0100

    Test nie przechodzi.

--- Calculator.Tests/CalculatorTests.cs
+++ Calculator.Tests/CalculatorTests.cs
@@ -1,8 +1,16 @@
-﻿using System;
+using System;
+using Xunit;
 
 namespace Calculator.Tests
 {
     public class CalculatorTests
     {
+        [Fact]
+        public void Add_AddsTwoPositiveNumbers_Calculated()
+        {
+            var calc = new Calculator();
+            int sum = calc.Add(2, 2);
+            Assert.Equal(4, sum);
+        }
     }
 }
--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -4,11 +4,9 @@ namespace Calculator
 {
     public class Calculator
     {
-
         public int Add(int a, int b)
         {
             throw new NotImplementedException();
         }
-
     }
 }
