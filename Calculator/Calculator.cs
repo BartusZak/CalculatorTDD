48467491ba5dd0537e97a31803a99f49555c700a
BartusZak <b.ploszynski@gmail.com>
2018-03-11 11:42:39 +0100

    Dodajemy klasę i metodę.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+
+        public int Add(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
