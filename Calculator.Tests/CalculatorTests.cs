5785d5c9855d2cec1e871a66f669eb29f55d3794
BartusZak <b.ploszynski@gmail.com>
2018-03-11 19:23:28 +0100

    Sklonowalem i dodalem 3 nowe testy.

--- Calculator.Tests/CalculatorTests.cs
+++ Calculator.Tests/CalculatorTests.cs
@@ -12,5 +12,29 @@ namespace Calculator.Tests
             int sum = calc.Add(2, 2);
             Assert.Equal(4, sum);
         }
+
+        [Fact]
+        public void Add_AddsTwoNegativeNumbers_Calculated()
+        {
+            var calc = new Calculator();
+            int sum = calc.Add((-2), (-2));
+            Assert.Equal((-4), sum);
+        }
+
+        [Fact]
+        public void Add_AddsPositiveToNegativeNumber_Calculated()
+        {
+            var calc = new Calculator();
+            int sum = calc.Add(2, (-2));
+            Assert.Equal(0, sum);
+        }
+
+        [Fact]
+        public void Add_AddsNegativeToPositiveNumber_Calculated()
+        {
+            var calc = new Calculator();
+            int sum = calc.Add((-2), 2);
+            Assert.Equal(0, sum);
+        }
     }
 }
