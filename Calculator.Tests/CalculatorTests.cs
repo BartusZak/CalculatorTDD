12b1218421684e593c3fddc8f81bac17463662ba
BartusZak <b.ploszynski@gmail.com>
2018-03-11 19:41:22 +0100

    Dodanie testów do metod.

--- Calculator.Tests/CalculatorTests.cs
+++ Calculator.Tests/CalculatorTests.cs
@@ -5,7 +5,39 @@ namespace Calculator.Tests
 {
     public class CalculatorTests
     {
-        
+        [Theory]
+        [InlineData(4, 2, 2.0f)]
+        [InlineData(-4, 2, -2.0f)]
+        [InlineData(4, -2, -2.0f)]
+        [InlineData(0, 3, 0.0f)]
+        [InlineData(5, 2, 2.5f)]
+        [InlineData(1, 3, 0.333333343f)]
+        public void Divide_ReturnsProperValue(int dividend, int divisor, float expectedQuotient)
+        {
+            var calc = new Calculator();
+            var quotient = calc.Divide(dividend, divisor);
+            Assert.Equal(expectedQuotient, quotient);
+        }
+
+        [Fact]
+        public void Divide_DivisionByZero_ThrowsException()
+        {
+            var calc = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calc.Divide(2, 0));
+        }
+
+        [Fact]
+        public void Divide_OnCalculatedEventIsCalled()
+        {
+            var calc = new Calculator();
+
+            bool wasEventCalled = false;
+            calc.CalculatedEvent += (sender, args) => wasEventCalled = true;
+
+            calc.Divide(1, 2);
+
+            Assert.True(wasEventCalled);
+        }
 
         [Fact]
         public void Add_AddsTwoPositiveNumbers_Calculated()
